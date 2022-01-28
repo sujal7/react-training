b55a45c3b65ea178854ad370b22af122e78cdd2c
Sujal Duwa <sujalduwa@gmail.com>
2022-01-28 19:07:04 +0545

    Added Assignment 2: Counter to increase and decrease value

---
+++ assignment2/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+// Create a component :
+// Must have counter: span
+// Buttons to decrease and increase counter
+// try setState with prev props
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Counter value="0" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
